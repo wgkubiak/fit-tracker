d3472dae0b4e26e3d62b3eaf4db80a44a3237383
wgkubiak <wgkubiak@gmail.com>
2019-05-06 00:31:11 +0200

    First commit - Working basic Fetch from API

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {apiResponse: ""}
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err)
+  }
+
+  componentDidMount() {
+    this.callAPI()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className="App-intro">{this.state.apiResponse}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
